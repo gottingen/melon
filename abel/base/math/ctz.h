f9702aa44c1fe09a094579e74383ec9390d31fd1
lijippy@163.com <lijippy@163.com>
2020-02-02 13:45:31 +0800

    bit options

--- abel/base/math/ctz.h
+++ abel/base/math/ctz.h
@@ -27,99 +27,99 @@ static ABEL_FORCE_INLINE unsigned ctz_template (Integral x) {
 /******************************************************************************/
 
 template<typename Integral>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros (Integral x);
+ABEL_FORCE_INLINE unsigned count_trailing_zeros (Integral x);
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<char> (char i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<char> (char i) {
     return ctz_template(i);
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned char> (unsigned char i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned char> (unsigned char i) {
     return ctz_template(i);
 }
 
 #if defined(ABEL_COMPILER_GNUC) || defined(ABEL_COMPILER_CLANG)
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned> (unsigned i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned> (unsigned i) {
     if (i == 0)
         return 8 * sizeof(i);
     return static_cast<unsigned>(__builtin_ctz(i));
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<int> (int i) {
-    return count_tailing_zeros(static_cast<unsigned>(i));
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<int> (int i) {
+    return count_trailing_zeros(static_cast<unsigned>(i));
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned long> (unsigned long i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned long> (unsigned long i) {
     if (i == 0)
         return 8 * sizeof(i);
     return static_cast<unsigned>(__builtin_ctzl(i));
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<long> (long i) {
-    return count_tailing_zeros(static_cast<unsigned long>(i));
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<long> (long i) {
+    return count_trailing_zeros(static_cast<unsigned long>(i));
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned long long> (unsigned long long i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned long long> (unsigned long long i) {
     if (i == 0)
         return 8 * sizeof(i);
     return static_cast<unsigned>(__builtin_ctzll(i));
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<long long> (long long i) {
-    return count_tailing_zeros(static_cast<unsigned long long>(i));
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<long long> (long long i) {
+    return count_trailing_zeros(static_cast<unsigned long long>(i));
 }
 
 #else
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<int> (int i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<int> (int i) {
     return ctz_template(i);
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned> (unsigned i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned> (unsigned i) {
     return ctz_template(i);
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<long> (long i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<long> (long i) {
     return ctz_template(i);
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned long> (unsigned long i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned long> (unsigned long i) {
     return ctz_template(i);
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<long long> (long long i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<long long> (long long i) {
     return ctz_template(i);
 }
 
-//! count_tailing_zeros (count trailing zeros)
+//! count_trailing_zeros (count trailing zeros)
 template<>
-ABEL_FORCE_INLINE unsigned count_tailing_zeros<unsigned long long> (unsigned long long i) {
+ABEL_FORCE_INLINE unsigned count_trailing_zeros<unsigned long long> (unsigned long long i) {
     return ctz_template(i);
 }
 
