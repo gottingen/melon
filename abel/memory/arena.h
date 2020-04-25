d62e8045b626dee2fe018399275ea5b544cd4e16
lijippy@163.com <lijippy@163.com>
2020-04-25 18:20:15 +0800

    add memory

---
+++ abel/memory/arena.h
@@ -0,0 +1,74 @@
+//
+// Created by liyinbin on 2020/3/30.
+//
+
+#ifndef ABEL_MEMORY_ARENA_H_
+#define ABEL_MEMORY_ARENA_H_
+
+
+#include <stdint.h>
+#include <abel/base/profile.h>
+
+namespace abel {
+
+    struct arena_options {
+        size_t initial_block_size;
+        size_t max_block_size;
+
+        // Constructed with default options.
+        arena_options();
+    };
+
+// Just a proof-of-concept, will be refactored in future CI.
+    class arena {
+    public:
+        explicit arena(const arena_options &options = arena_options());
+
+        ~arena();
+
+        void swap(arena &);
+
+        void *allocate(size_t n);
+
+        void *allocate_aligned(size_t n);  // not implemented.
+        void clear();
+
+    private:ABEL_NON_COPYABLE(arena);
+
+        struct block {
+            uint32_t left_space() const { return size - alloc_size; }
+
+            block *next;
+            uint32_t alloc_size;
+            uint32_t size;
+            char data[0];
+        };
+
+        void *allocate_in_other_blocks(size_t n);
+
+        void *allocate_new_block(size_t n);
+
+        block *pop_block(block *&head) {
+            block *saved_head = head;
+            head = head->next;
+            return saved_head;
+        }
+
+        block *_cur_block;
+        block *_isolated_blocks;
+        size_t _block_size;
+        arena_options _options;
+    };
+
+    inline void *arena::allocate(size_t n) {
+        if (_cur_block != nullptr && _cur_block->left_space() >= n) {
+            void *ret = _cur_block->data + _cur_block->alloc_size;
+            _cur_block->alloc_size += n;
+            return ret;
+        }
+        return allocate_in_other_blocks(n);
+    }
+
+}  // namespace abel
+
+#endif //ABEL_MEMORY_ARENA_H_
---
+++ abel/memory/leaky_singleton.h
@@ -0,0 +1,61 @@
+//
+// Created by liyinbin on 2020/4/7.
+//
+
+#ifndef ABEL_MEMORY_LEAKY_SINGLETON_H_
+#define ABEL_MEMORY_LEAKY_SINGLETON_H_
+
+
+#include <pthread.h>
+#include <atomic>
+#include <abel/asl/functional.h>
+
+namespace abel {
+
+    template<typename T>
+    class leaky_singleton {
+    public:
+        static std::atomic<uintptr_t> g_leaky_singleton_untyped;
+        static abel::once_flag g_create_leaky_singleton_once;
+
+        static void create_leaky_singleton();
+    };
+
+    template<typename T>
+    std::atomic<uintptr_t> leaky_singleton<T>::g_leaky_singleton_untyped{0};
+    template<typename T>
+    abel::once_flag leaky_singleton<T>::g_create_leaky_singleton_once;
+
+    template<typename T>
+    void leaky_singleton<T>::create_leaky_singleton() {
+        T *obj = new T;
+        leaky_singleton<T>::g_leaky_singleton_untyped.store(reinterpret_cast<uintptr_t>(obj));
+    }
+
+// To get a never-deleted singleton of a type T, just call get_leaky_singleton<T>().
+// Most daemon threads or objects that need to be always-on can be created by
+// this function.
+// This function can be called safely before main() w/o initialization issues of
+// global variables.
+    template<typename T>
+    inline T *get_leaky_singleton() {
+        const uintptr_t value = leaky_singleton<T>::g_leaky_singleton_untyped.load();
+        if (value) {
+            return reinterpret_cast<T *>(value);
+        }
+        abel::call_once(leaky_singleton<T>::g_create_leaky_singleton_once,
+                        leaky_singleton<T>::create_leaky_singleton);
+        return reinterpret_cast<T *>(leaky_singleton<T>::g_leaky_singleton_untyped.load());
+    }
+
+// True(non-NULL) if the singleton is created.
+// The returned object (if not NULL) can be used directly.
+    template<typename T>
+    inline T *has_leaky_singleton() {
+        return reinterpret_cast<T *>(
+                leaky_singleton<T>::g_leaky_singleton_untyped.load());
+    }
+
+} // namespace abel
+
+#endif //ABEL_MEMORY_LEAKY_SINGLETON_H_
