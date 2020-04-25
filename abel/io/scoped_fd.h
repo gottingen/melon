70cd91e19671bdce5dab238ee5f41088717aa8fc
lijippy@163.com <lijippy@163.com>
2020-04-25 17:24:42 +0800

    add io

---
+++ abel/io/scoped_fd.h
@@ -0,0 +1,54 @@
+//
+// Created by liyinbin on 2020/3/30.
+//
+
+#ifndef ABEL_IO_SCOPED_FD_H_
+#define ABEL_IO_SCOPED_FD_H_
+
+#include <unistd.h>
+#include <abel/base/profile.h>
+
+namespace abel {
+
+    class scoped_fd {
+    public:
+        scoped_fd() : _fd(-1) {}
+
+        explicit scoped_fd(int fd) : _fd(fd) {}
+
+        ~scoped_fd() {
+            if (_fd >= 0) {
+                ::close(_fd);
+                _fd = -1;
+            }
+        }
+
+        void reset(int fd) {
+            if (_fd >= 0) {
+                ::close(_fd);
+                _fd = -1;
+            }
+            _fd = fd;
+        }
+
+        /**
+         * set fd to -1
+         * @return
+         */
+        int release() {
+            const int prev_fd = _fd;
+            _fd = -1;
+            return prev_fd;
+        }
+
+        operator int() const { return _fd; }
+
+    private:
+        ABEL_NON_COPYABLE(scoped_fd);
+        int _fd;
+    };
+
+
+} //namespace abel
+
+#endif //ABEL_IO_SCOPED_FD_H_
