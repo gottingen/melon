25a7a5cdc1ca8ce5512b0b94ee2f4ac79586f807
lijippy@163.com <lijippy@163.com>
2021-04-27 16:58:33 +0800

    fix once call

--- abel/memory/deep_copy_ptr.h
+++ abel/memory/deep_copy_ptr.h
@@ -8,44 +8,59 @@
 
 namespace abel {
 
-template<typename T>
-class deep_copy_ptr {
-  public:
-    deep_copy_ptr() : _ptr(NULL) {}
+    template<typename T>
+    class deep_copy_ptr {
+    public:
+        deep_copy_ptr() : _ptr(NULL) {}
 
-    explicit deep_copy_ptr(T *obj) : _ptr(obj) {}
+        explicit deep_copy_ptr(T *obj) : _ptr(obj) {}
 
-    ~deep_copy_ptr() {
-        delete _ptr;
-    }
-
-    deep_copy_ptr(const deep_copy_ptr &rhs)
-            : _ptr(rhs._ptr ? new T(*rhs._ptr) : NULL) {}
-
-    void operator=(const deep_copy_ptr &rhs) {
-        if (rhs._ptr) {
-            if (_ptr) {
-                *_ptr = *rhs._ptr;
-            } else {
-                _ptr = new T(*rhs._ptr);
-            }
-        } else {
+        ~deep_copy_ptr() {
             delete _ptr;
-            _ptr = NULL;
         }
+
+        deep_copy_ptr(const deep_copy_ptr &rhs)
+                : _ptr(rhs._ptr ? new T(*rhs._ptr) : NULL) {}
+
+        void operator=(const deep_copy_ptr &rhs) {
+            if (rhs._ptr) {
+                if (_ptr) {
+                    *_ptr = *rhs._ptr;
+                } else {
+                    _ptr = new T(*rhs._ptr);
+                }
+            } else {
+                delete _ptr;
+                _ptr = NULL;
+            }
+        }
+
+        T *get() const { return _ptr; }
+
+        void reset(T *ptr) {
+            delete _ptr;
+            _ptr = ptr;
+        }
+
+        operator void *() const { return _ptr; }
+
+    private:
+        T *_ptr;
+    };
+
+
+    namespace memory_internal {
+
+        template<class T, class Tag = void>
+        struct early_init_instance {
+            inline static non_destroy <T> object;
+        };
+
+    }  // namespace detail
+
+    template<class T, class Tag = void>
+    const T &early_init_constant() {
+        return *memory_internal::early_init_instance<T, Tag>::object.get();
     }
-
-    T *get() const { return _ptr; }
-
-    void reset(T *ptr) {
-        delete _ptr;
-        _ptr = ptr;
-    }
-
-    operator void *() const { return _ptr; }
-
-  private:
-    T *_ptr;
-};
 }  // namespace abel
 #endif  // ABEL_MEMORY_DEEP_COPY_PTR_H_
