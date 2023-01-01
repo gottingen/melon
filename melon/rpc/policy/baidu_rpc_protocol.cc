2e8ba32ae06d32f963fd27e4efffb2eecb2644db
lijippy@163.com <lijippy@163.com>
2023-01-02 06:46:07 +0800

    Fix a bug that server will send unexpected data frame to client if there are errors occur during processing stream create request

--- melon/rpc/policy/baidu_rpc_protocol.cc
+++ melon/rpc/policy/baidu_rpc_protocol.cc
@@ -231,10 +231,12 @@ namespace melon::rpc {
             if (span) {
                 span->set_response_size(res_buf.size());
             }
-            if (stream_ptr) {
-                MELON_CHECK(accessor.remote_stream_settings() != nullptr);
+            // Send rpc response over stream even if server side failed to create
+            // stream for some reasons.
+            if(cntl->has_remote_stream()){
                 // Send the response over stream to notify that this stream connection
                 // is successfully built.
+                // Response_stream can be INVALID_STREAM_ID when error occurs.
                 if (SendStreamData(sock, &res_buf,
                                    accessor.remote_stream_settings()->stream_id(),
                                    accessor.response_stream()) != 0) {
@@ -242,13 +244,17 @@ namespace melon::rpc {
                     MELON_PLOG_IF(WARNING, errcode != EPIPE) << "Fail to write into " << *sock;
                     cntl->SetFailed(errcode, "Fail to write into %s",
                                     sock->description().c_str());
-                    ((Stream *) stream_ptr->conn())->Close();
+                    if(stream_ptr) {
+                        ((Stream*)stream_ptr->conn())->Close();
+                    }
                     return;
                 }
-                // Now it's ok the mark this server-side stream as connectted as all the
-                // written user data would follower the RPC response.
-                ((Stream *) stream_ptr->conn())->SetConnected();
-            } else {
+                if(stream_ptr) {
+                    // Now it's ok the mark this server-side stream as connectted as all the
+                    // written user data would follower the RPC response.
+                    ((Stream*)stream_ptr->conn())->SetConnected();
+                }
+            } else{
                 // Have the risk of unlimited pending responses, in which case, tell
                 // users to set max_concurrency.
                 Socket::WriteOptions wopt;
@@ -558,18 +564,19 @@ namespace melon::rpc {
 
             const fiber_token_t cid = {static_cast<uint64_t>(meta.correlation_id())};
             Controller *cntl = nullptr;
+            StreamId remote_stream_id = meta.has_stream_settings() ? meta.stream_settings().stream_id(): INVALID_STREAM_ID;
             const int rc = fiber_token_lock(cid, (void **) &cntl);
             if (rc != 0) {
                 MELON_LOG_IF(ERROR, rc != EINVAL && rc != EPERM)
                                 << "Fail to lock correlation_id=" << cid << ": " << melon_error(rc);
-                if (meta.has_stream_settings()) {
+                if (remote_stream_id != INVALID_STREAM_ID) {
                     SendStreamRst(msg->socket(), meta.stream_settings().stream_id());
                 }
                 return;
             }
 
             ControllerPrivateAccessor accessor(cntl);
-            if (meta.has_stream_settings()) {
+            if (remote_stream_id != INVALID_STREAM_ID) {
                 accessor.set_remote_stream_settings(
                         new StreamSettings(meta.stream_settings()));
             }
